79673f2c04af600a04765476edd9d181234dbd8d
Kgotso <mahipsafet@gmail.com>
2016-10-09 00:12:03 +0200

    Added css styles to (step7)

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -4,7 +4,7 @@ from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
-
+app.config.from_object(__name__)
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
@@ -12,7 +12,7 @@ app.config.update(dict(
     USERNAME='admin',
     PASSWORD='default'
 ))
-#app.config.from_object(__name__)
+
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
